64310b833c12dfcf7b16c9c63b7614acae0b4e52
Showernugget <zak.andrews1603@gmail.com>
2024-09-05 18:02:59 +1200

    Add files via upload

--- stays/admin.py
+++ stays/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Stay, Host
 
 # Register your models here.
+admin.site.register(Stay)
+admin.site.register(Host)
---
+++ stays/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import StayListView, StayDetailView, HostListView, HostDetailView
+
+# urls for stays
+urlpatterns = [
+    path("", StayListView.as_view(), name='home'),
+    path('stays/', StayListView.as_view(), name='stay_list'),
+    path('stays/<int:pk>', StayDetailView.as_view(), name='stay_detail'),
+    path('hosts/', HostListView.as_view(), name='host_list'),
+    path('hosts/<int:pk>', HostDetailView.as_view(), name='host_detail'),
+]
