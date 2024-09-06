98ccb3e67b2db26ee2e3c95f68256abb321aacd9
jedi24garcia <jedi24garcia@gmail.com>
2024-09-06 13:22:27 +1200

    base template partially done

--- experiences/views.py
+++ experiences/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def search(request):
+    query = request.GET.get('q')
+    return render(request, 'search_results.html', {'query': query})
+
 def home(request):
     return render(request, 'home.html')
 
