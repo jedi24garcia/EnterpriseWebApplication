a74248cbea4e27a9005aea1bde0e4dad1b1a972f
Showernugget <zak.andrews1603@gmail.com>
2024-09-16 14:54:24 +1200

    Review and Booking setup
    Models according to the data model, views, urls, and empty templates added for reviews app and booking app

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from stays.models import Stay
 
 # Create your models here.
+# Review model
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
+    property = models.ForeignKey(Stay, on_delete=models.PROTECT, null=True)
+    rating = models.IntegerField(blank=True, null=True)
+    comments = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.property.title
