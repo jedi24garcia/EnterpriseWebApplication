c0cdb0b02d80b6202dc60744e3cf0ac618bad699
simonstudy2024 <simonstudy2024@gmail.com>
2024-09-22 07:38:57 +1200

    fixed hash tag

--- accounts/models.py
+++ accounts/models.py
@@ -34,7 +34,7 @@ def create_profile(sender, instance, created, **kwargs):
         user_profile = Profile(user=instance)
         user_profile.save()
        
-Automate the profile thing
+#Automate the profile thing
 post_save.connect(create_profile, sender=User)
 
 
